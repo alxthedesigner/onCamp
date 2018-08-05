4da15e90bd41cdb4f060540d1085ecd3feb96fae
Alexandra Stanford <alxstan00@yahoo.com>
2018-08-05 14:00:13 -0400

    Initial Commit

---
+++ CampusFoodDeliveryApp/loginScreenViewController.swift
@@ -0,0 +1,27 @@
+//
+//  loginScreenViewController.swift
+//  CampusFoodDeliveryApp
+//
+//  Created by Alex Stanford on 8/3/18.
+//  Copyright © 2018 Alex Stanford. All rights reserved.
+//
+
+import UIKit
+
+class loginScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+
+}
